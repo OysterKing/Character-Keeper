5ffb7af8b34e8f254d833031c94d1604c0b3603c
OysterKing <comhghall.mc-keating@ucd.ie>
2014-08-16 14:01:20 +0100

    Character tester, accidentally ommitted from previous commit

---
+++ CharacterTester.java
@@ -0,0 +1,13 @@
+
+public class CharacterTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PlayerCharacter Grell = new PlayerCharacter("Grell", "Human", "Barbarian", 5, 33, 198, 95);
+		Grell.print_basic_info();
+		
+
+	}
+
+}
