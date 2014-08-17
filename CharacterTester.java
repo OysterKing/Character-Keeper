d97d3b89dc895602e6a10c9cf850cde19cdacb2d
OysterKing <comhghall.mc-keating@ucd.ie>
2014-08-17 12:05:22 +0100

    Added an ability class and corresponding fucntions to calculate ability modifiers.

--- CharacterTester.java
+++ CharacterTester.java
@@ -6,8 +6,12 @@ public class CharacterTester {
 		
 		PlayerCharacter Grell = new PlayerCharacter("Grell", "Human", "Barbarian", 5, 33, 198, 95);
 		Grell.print_basic_info();
-		
-
+		Grell.abilities[0].printAbility();
+		Grell.abilities[1].printAbility();
+		Grell.abilities[2].printAbility();
+		Grell.abilities[3].printAbility();
+		Grell.abilities[4].printAbility();
+		Grell.abilities[5].printAbility();
 	}
 
 }
--- PlayerCharacter.java
+++ PlayerCharacter.java
@@ -6,6 +6,7 @@
 public class PlayerCharacter {
 	String name, race, character_class;
 	int level, age, height, weight;
+	Ability[] abilities = new Ability[6];
 	
 	public PlayerCharacter(String n, String r, String char_class, int lvl, int a, int h, int w){
 			name = n;
@@ -16,16 +17,70 @@ public class PlayerCharacter {
 			height = h;
 			weight = w;
 			
+			for(int i = 0; i < abilities.length; i++){
+				if(i == 0){
+					abilities[i] = new Ability("Strength", 18);
+					abilities[i].calculateMod();
+				}
+				
+				if(i == 1){
+					abilities[i] = new Ability("Constitution", 18);
+					abilities[i].calculateMod();
+				}
+				
+				if(i == 2){
+					abilities[i] = new Ability("Dexterity", 15);
+					abilities[i].calculateMod();
+				}
+				
+				if(i == 3){
+					abilities[i] = new Ability("Intelligence", 10);
+					abilities[i].calculateMod();
+				}
+				
+				if(i == 4){
+					abilities[i] = new Ability("Wisdom", 12);
+					abilities[i].calculateMod();
+				}
+				
+				if(i == 5){
+					abilities[i] = new Ability("Charisma", 8);
+					abilities[i].calculateMod();
+				}
+					
+			}
 	}
 	
 	public void print_basic_info(){
-			System.out.println("Name: " + name);
-			System.out.println("Race: " + race);
-			System.out.println("Class: " + character_class);
-			System.out.println("Level: " + level);
-			System.out.println("Age: " + age);
-			System.out.println("Height(cm): " + height);
-			System.out.println("Weight(kilo): " + weight);
+		System.out.println("Name: " + name);
+		System.out.println("Race: " + race);
+		System.out.println("Class: " + character_class);
+		System.out.println("Level: " + level);
+		System.out.println("Age: " + age);
+		System.out.println("Height(cm): " + height);
+		System.out.println("Weight(kilo): " + weight);
+	}
+	
+	public class Ability{
+			private String ability_name;
+			int value;
+			private int modifier;
+			private int lowest_mod = -4;
+			private int lowest_value = 2;
+			
+			public Ability(String name, int val){
+				ability_name = name;
+				value = val;
+			}
+			
+			public void calculateMod(){
+					int temp = value - lowest_value;
+					modifier = lowest_mod + temp/2 + level/2;
+			}
+			
+			public void printAbility(){
+				System.out.println(ability_name + ": " + value + " -> " + modifier);
+			}
 	}
 	
 }
