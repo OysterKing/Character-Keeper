09d1904e6a66c705e5e7f3b3a440a3640ed8d7ff
OysterKing <comhghalldavid@live.com>
2014-08-25 11:52:32 +0100

    Implemented racial bonuses. The CreatePlayerCharacterClass now reads the basic information file and checks to see what race the user has selected and adds the appropriate ability modifiers.

--- CreatePlayerCharacter.java
+++ CreatePlayerCharacter.java
@@ -1,4 +1,3 @@
-
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.Scanner;
@@ -9,21 +8,21 @@ import java.util.Scanner;
  */
 
 public class CreatePlayerCharacter {
-	
+
 	String newCharacter;
-	String tempRace;
-	
+	//	String tempRace;
+
 	public CreatePlayerCharacter(String newCharacterName){
 		newCharacter = newCharacterName;
 	}
-	
+
 	public void create(){
 		try{
 			FileReader readFile = new FileReader(newCharacter + "BasicInfo.txt");
 			Scanner in = new Scanner(readFile);
 			PlayerCharacter Grell = new PlayerCharacter(in.nextLine(), in.nextLine(), in.nextLine(), 
 					Integer.parseInt(in.nextLine()), Integer.parseInt(in.nextLine()), Integer.parseInt(in.nextLine()));
-			
+
 			Grell.print_basic_info();
 			System.out.println();
 			Grell.printAbilityTable();
@@ -33,215 +32,241 @@ public class CreatePlayerCharacter {
 			System.out.println("Exception " + ex.getMessage() + " caught in create.");
 		}
 	}
-	
+
 	public void enterBasicInfo(){
 		String[] basicInfo = new String[6];
-		
+
 		basicInfo[0] = "Name";
 		basicInfo[1] = "Race";
 		basicInfo[2] = "Class";
 		basicInfo[3] = "Age";
 		basicInfo[4] = "Height(cm)";
 		basicInfo[5] = "Weight(kg)";
-		
+
 		try{
 			PrintWriter out = new PrintWriter(newCharacter + "BasicInfo.txt");
 			Scanner in = new Scanner(System.in);
-			
+
 			for(int i = 0; i < 6; i++){
 				System.out.println("Enter " + basicInfo[i] + ": ");
-				
+
 				if(i == 1){
-//					tempRace = in.nextLine(); //need to use the race to adjust ability scores accordingly
-//					out.flush();
-//					out.println(tempRace);
-					
-					tempRace = "Human";
+					//					tempRace = in.nextLine(); //need to use the race to adjust ability scores accordingly
+					//					out.flush();
+					//					out.println(tempRace);
+
+					//					tempRace = "Human";
 					out.println(in.nextLine());
 				}
-				
+
 				else{
 					out.println(in.nextLine());
 				}
 			}
-			
+
 			out.close();
-			
+
 		}catch(Exception ex){
 			System.out.println("Exception " + ex.getMessage() + " caught in enterBasicInfo");
 		}
-		
+
 	}
-	
+
 	public void enterValues(){ 
 		Scanner in = new Scanner(System.in);
-		
+
 		int temp;
-		
+
 		String[] abilityNames = new String[6];
-		
+
 		abilityNames[0] = "STRENGTH";
 		abilityNames[1] = "CONSTITUTION";
 		abilityNames[2] = "DEXTERITY";
 		abilityNames[3] = "INTELLIGENCE";
 		abilityNames[4] = "WISDOM";
 		abilityNames[5] = "CHARISMA";
-		
+
 		try{
 			PrintWriter out = new PrintWriter(newCharacter + "Attributes.txt");
-			
-			if(tempRace == "Dragonborn"){ //+2 strength and charisma
-				for(int i = 0; i < 6; i++){
-					
-					System.out.println("Enter base " + abilityNames[i] + ": ");
-					
-					if(i == 0 || i == 5){
-						temp = in.nextInt();
-						temp += 2;
-						out.println(temp);
-					}
+			FileReader readFile = new FileReader(newCharacter + "BasicInfo.txt");
+			Scanner inFile = new Scanner(readFile);
+
+			while(inFile.hasNextLine()){
 				
-					else{
-						out.println(in.nextInt());	
+				System.out.println("CHECK");
+				String line = inFile.nextLine();
+
+				if("Dragonborn".equals(line)){ //+2 strength and charisma
+					for(int i = 0; i < 6; i++){
+
+						System.out.println("Enter base " + abilityNames[i] + ": ");
+
+						if(i == 0 || i == 5){
+							temp = in.nextInt();
+							temp += 2;
+							out.println(temp);
+						}
+
+						else{
+							out.println(in.nextInt());	
+						}
 					}
 				}
-			}
-			
-			else if(tempRace == "Dwarf"){ //+2 constitution and wisdom
-				for(int i = 0; i < 6; i++){
 
-					System.out.println("Enter base " + abilityNames[i] + ": ");
 
-					if(i == 1 || i == 4){
-						temp = in.nextInt();
-						temp += 2;
-						out.println(temp);
-					}
 
-					else{
-						out.println(in.nextInt());	
-					}
-				}
-			}
-			
-			else if(tempRace == "Eladrin"){ //+2 intelligence and dexterity
-				for(int i = 0; i < 6; i++){
 
-					System.out.println("Enter base " + abilityNames[i] + ": ");
+				else if("Dwarf".equals(line)){ //+2 constitution and wisdom
+					for(int i = 0; i < 6; i++){
 
-					if(i == 2 || i == 3){
-						temp = in.nextInt();
-						temp += 2;
-						out.println(temp);
-					}
+						System.out.println("Enter base " + abilityNames[i] + ": ");
 
-					else{
-						out.println(in.nextInt());	
+						if(i == 1 || i == 4){
+							temp = in.nextInt();
+							temp += 2;
+							out.println(temp);
+						}
+
+						else{
+							out.println(in.nextInt());	
+						}
 					}
 				}
-			}
-			
-			else if(tempRace == "Elf"){ //+2 dexterity and wisdom
-				for(int i = 0; i < 6; i++){
 
-					System.out.println("Enter base " + abilityNames[i] + ": ");
 
-					if(i == 2 || i == 4){
-						temp = in.nextInt();
-						temp += 2;
-						out.println(temp);
-					}
 
-					else{
-						out.println(in.nextInt());	
-					}
-				}
-			}
-			
-			else if(tempRace == "Half-Elf"){ //+2 constitution and charisma
-				for(int i = 0; i < 6; i++){
 
-					System.out.println("Enter base " + abilityNames[i] + ": ");
 
-					if(i == 1 || i == 5){
-						temp = in.nextInt();
-						temp += 2;
-						out.println(temp);
-					}
+				else if("Eladrin".equals(line)){ //+2 intelligence and dexterity
+					for(int i = 0; i < 6; i++){
 
-					else{
-						out.println(in.nextInt());	
+						System.out.println("Enter base " + abilityNames[i] + ": ");
+
+						if(i == 2 || i == 3){
+							temp = in.nextInt();
+							temp += 2;
+							out.println(temp);
+						}
+
+						else{
+							out.println(in.nextInt());	
+						}
 					}
 				}
-			}
-			
-			else if(tempRace == "Halfling"){ //+2 dexterity and charisma
-				for(int i = 0; i < 6; i++){
 
-					System.out.println("Enter base " + abilityNames[i] + ": ");
 
-					if(i == 2 || i == 5){
-						temp = in.nextInt();
-						temp += 2;
-						out.println(temp);
-					}
 
-					else{
-						out.println(in.nextInt());	
-					}
-				}
-			}
-			
-			if(tempRace == "Human"){ //+2 to one attribute of your choice
-				
-				int choiceAttribute;
-				
-				System.out.println("Choose attribute for racial bonus (+2): ");
-				for(int i = 0; i < 6; i++){
-					System.out.println(abilityNames[i] + ": " + i);
-				}
-				
-				choiceAttribute = in.nextInt();
-				System.out.println();
-				
-				for(int i = 0; i < 6; i++){
+				else if("Elf".equals(line)){ //+2 dexterity and wisdom
+					for(int i = 0; i < 6; i++){
 
-					System.out.println("Enter base " + abilityNames[i] + ": ");
+						System.out.println("Enter base " + abilityNames[i] + ": ");
 
-					if(i == choiceAttribute){
-						temp = in.nextInt();
-						temp += 2;
-						out.println(temp);
-					}
+						if(i == 2 || i == 4){
+							temp = in.nextInt();
+							temp += 2;
+							out.println(temp);
+						}
 
-					else{
-						out.println(in.nextInt());	
+						else{
+							out.println(in.nextInt());	
+						}
 					}
 				}
-			}
-			
-			if(tempRace == "Tiefling"){ //+2 to intelligence and charisma
-				for(int i = 0; i < 6; i++){
 
-					System.out.println("Enter base " + abilityNames[i] + ": ");
 
-					if(i == 3 || i == 5){
-						temp = in.nextInt();
-						temp += 2;
-					}
 
-					else{
-						out.println(in.nextInt());	
+				else if("Half-Elf".equals(line)){ //+2 constitution and charisma
+					for(int i = 0; i < 6; i++){
+
+						System.out.println("Enter base " + abilityNames[i] + ": ");
+
+						if(i == 1 || i == 5){
+							temp = in.nextInt();
+							temp += 2;
+							out.println(temp);
+						}
+
+						else{
+							out.println(in.nextInt());	
+						}
 					}
 				}
-			}
-			
+
+
+
+				else if("Halfling".equals(line)){ //+2 dexterity and charisma
+					for(int i = 0; i < 6; i++){
+
+						System.out.println("Enter base " + abilityNames[i] + ": ");
+
+						if(i == 2 || i == 5){
+							temp = in.nextInt();
+							temp += 2;
+							out.println(temp);
+						}
+
+						else{
+							out.println(in.nextInt());	
+						}
+					}
+				}
+
+
+
+				else if("Human".equals(line)){ //+2 to one attribute of your choice
+
+					int choiceAttribute;
+
+					System.out.println("Choose attribute for racial bonus (+2): ");
+					for(int i = 0; i < 6; i++){
+						System.out.println(abilityNames[i] + ": " + i);
+					}
+
+					choiceAttribute = in.nextInt();
+					System.out.println();
+
+					for(int i = 0; i < 6; i++){
+
+						System.out.println("Enter base " + abilityNames[i] + ": ");
+
+						if(i == choiceAttribute){
+							temp = in.nextInt();
+							temp += 2;
+							out.println(temp);
+						}
+
+						else{
+							out.println(in.nextInt());	
+						}
+					}
+				}
+
+
+
+				else if("Tiefling".equals(line)){ //+2 to intelligence and charisma
+					for(int i = 0; i < 6; i++){
+
+						System.out.println("Enter base " + abilityNames[i] + ": ");
+
+						if(i == 3 || i == 5){
+							temp = in.nextInt();
+							temp += 2;
+						}
+
+						else{
+							out.println(in.nextInt());	
+						}
+					}
+				}
+			}	
+
 			out.close();
-			
+			inFile.close();
+
 		}catch(Exception ex){
 			System.out.println("Exception " + ex.getMessage() + " caught in enter attributes.");
 		}	
-		
+
 	}
 
 }
